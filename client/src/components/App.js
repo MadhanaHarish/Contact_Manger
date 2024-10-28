d97fe299be94f3ffd1b0321a0f355d9335a17a34
Madhana Harish <126047299+MadhanaHarish@users.noreply.github.com>
2024-10-28 06:11:51 +0530

    Update App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import React, {useState, useEffect} from "react";
+import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import axios from "axios";
 import "./App.css";
 import Header from "./Header";
-import AddContact from "./AddContact";
-import EditContact from "./EditContact";
-import ContactList from "./ContactList";
-import DarkMode from "./darkmode";
+import AddContact from "./AddContact.js";
+import EditContact from "./EditContact.js";
+import ContactList from "./ContactList.js";
+import DarkMode from "./darkmode.js";
 import ContactDetail from "./ContactDetail";
-import Login from "./Login";
 
 function App() {
     const [contacts, setContacts] = useState([]);
     const [searchTerm, setSearchTerm] = useState("");
     const [searchResults, setSearchResults] = useState([]);
-    const [isAuthenticated, setIsAuthenticated] = useState(false); // New state for login status
-
     const getContacts = async () => {
         try {
             const res = await axios.get("/api/contacts");
@@ -70,52 +67,41 @@ function App() {
     useEffect(() => {
         getContacts();
     }, []);
-
-    const handleLogin = () => {
-        setIsAuthenticated(true);
-    };
-
     return (
         <div className="App-container">
             <Router>
-                {!isAuthenticated ? (
-                    <Login onLogin={handleLogin} /> // Show login page if not authenticated
-                ) : (
-                    <>
-                        <Header/>
-                        <Routes>
-                            <Route
-                                path="/"
-                                element={
-                                    <ContactList
-                                        contacts={searchTerm.length < 1 ? contacts : searchResults}
-                                        getContactID={removeContactHandler}
-                                        term={searchTerm}
-                                        searchKeyword={searchHandler}
-                                    />
-                                }
+                <Header/>
+                <Routes>
+                    <Route
+                        path="/"
+                        element={
+                            <ContactList
+                                contacts={searchTerm.length < 1 ? contacts : searchResults}
+                                getContactID={removeContactHandler}
+                                term={searchTerm}
+                                searchKeyword={searchHandler}
                             />
-                            <Route
-                                path="/add"
-                                element={<AddContact addContactHandler={addContactHandler}/>}
+                        }
+                    />
+                    <Route
+                        path="/add"
+                        element={<AddContact addContactHandler={addContactHandler}/>}
+                    />
+                    <Route
+                        path="/edit/:id"
+                        element={
+                            <EditContact
+                                contacts={contacts}
+                                updateContactHandler={updateContactHandler}
                             />
-                            <Route
-                                path="/edit/:id"
-                                element={
-                                    <EditContact
-                                        contacts={contacts}
-                                        updateContactHandler={updateContactHandler}
-                                    />
-                                }
-                            />
-                            <Route
-                                path="/contact/:id"
-                                element={<ContactDetail contacts={contacts}/>}
-                            />
-                        </Routes>
-                        <DarkMode/>
-                    </>
-                )}
+                        }
+                    />
+                    <Route
+                        path="/contact/:id"
+                        element={<ContactDetail contacts={contacts}/>}
+                    />
+                </Routes>
+                <DarkMode/>
             </Router>
         </div>
     );
